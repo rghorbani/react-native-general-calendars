4e910c4231c7b9c43413722817283dfb1fdf07a0
Reza <r.ghorbani.f@gmail.com>
2019-03-30 14:45:27 +0430

    change lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
   // },
   rules: {
     // "react/display-name": 0,
-    // "react/prop-types": 0,
-  }
+    'react/prop-types': 0,
+    'react-native/no-inline-styles': 0,
+  },
 };
